8dd47b2f932b8b3ac9c31999bb7a8c69a03a2728
UPMHJohan <233110539@upmh.edu.mx>
2026-06-18 21:06:23 -0600

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,14 +4,16 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(localStorage.getItem("token") || null);
-  const [usuario, setUsuario] = useState(null);
-  const [rol, setRol] = useState(null);
+  const [usuario, setUsuario] = useState(localStorage.getItem("usuario") || null);
+  const [rol, setRol] = useState(localStorage.getItem("rol") || null);
 
   const login = (tokenRecibido, datosUsuario) => {
     setToken(tokenRecibido);
     setUsuario(datosUsuario.nombre);
     setRol(datosUsuario.rol);
     localStorage.setItem("token", tokenRecibido);
+    localStorage.setItem("usuario", datosUsuario.nombre);
+    localStorage.setItem("rol", datosUsuario.rol);
   };
 
   const logout = () => {
@@ -19,6 +21,8 @@ export function AuthProvider({ children }) {
     setUsuario(null);
     setRol(null);
     localStorage.removeItem("token");
+    localStorage.removeItem("usuario");
+    localStorage.removeItem("rol");
   };
 
   return (
