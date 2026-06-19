6d862f1fd8be3f34c585468bed958753463d7932
redhollow8959-oss <redhollow8959@gmail.com>
2026-06-19 17:52:17 -0600

    agregar PATCH, status 204/409, alias progreso y endpoint materiales.

--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,6 +1,6 @@
 const express     = require('express');
 const router      = express.Router();
-const { getCursos, getCursoById, crearCurso, actualizarCurso, eliminarCurso } = require('../controllers/courseController');
+const { getCursos, getCursoById, crearCurso, actualizarCurso, eliminarCurso, getMaterialesLeccion } = require('../controllers/courseController');
 const verifyToken = require('../middleware/verifyToken');
 const checkRole   = require('../middleware/checkRole');
 
--- backend/src/routes/quizRoutes.js
+++ backend/src/routes/quizRoutes.js
@@ -1,6 +1,6 @@
 const express     = require('express');
 const router      = express.Router();
-const { getQuizById, submitQuiz, getResultado } = require('../controllers/quizController');
+const { submitQuiz, getResultado, getQuizById, getQuizzesByCurso } = require('../controllers/quizController');
 const verifyToken = require('../middleware/verifyToken');
 const checkRole   = require('../middleware/checkRole');
 
